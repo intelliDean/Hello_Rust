f8ddc2ef4822c0461351068a523c6a4b90628174
Dean <o.michaeldean@gmail.com>
2025-03-23 23:11:25 +0100

    updated

---
+++ target/debug/deps/hello_rust-d042c63417f4e258.d
@@ -0,0 +1,5 @@
+/home/dean/RustProjects/hello_rust/target/debug/deps/libhello_rust-d042c63417f4e258.rmeta: src/lib.rs
+
+/home/dean/RustProjects/hello_rust/target/debug/deps/hello_rust-d042c63417f4e258.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/debug/deps/hello_rust-d6ecd8d97631cbdd.d
@@ -0,0 +1,13 @@
+/home/dean/RustProjects/hello_rust/target/debug/deps/hello_rust-d6ecd8d97631cbdd: src/main.rs src/primitive_data_types.rs src/compound_data_types.rs src/functions.rs src/ownership.rs src/borrowing.rs src/struct_borrow.rs src/var_const.rs src/control_flow.rs
+
+/home/dean/RustProjects/hello_rust/target/debug/deps/hello_rust-d6ecd8d97631cbdd.d: src/main.rs src/primitive_data_types.rs src/compound_data_types.rs src/functions.rs src/ownership.rs src/borrowing.rs src/struct_borrow.rs src/var_const.rs src/control_flow.rs
+
+src/main.rs:
+src/primitive_data_types.rs:
+src/compound_data_types.rs:
+src/functions.rs:
+src/ownership.rs:
+src/borrowing.rs:
+src/struct_borrow.rs:
+src/var_const.rs:
+src/control_flow.rs:
